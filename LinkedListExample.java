b69fbfb783a3cdd1f5de2fe48dfaf12c32520743
alvinmjosan <alvinmjosan@gmail.com>
2026-01-19 09:30:11 +0530

    added LinkedListExample.java

---
+++ LinkedListExample.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+public class LinkedListExample{
+    public static void main(String[]args){
+        LinkedList<String>item=new LinkedList<String>();
+        item.add("car");
+        item.add("bike");
+        item.add("cycle");
+        System.out.println(item);
+    }
+}
